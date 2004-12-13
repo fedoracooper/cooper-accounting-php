d5a74bc6e85ad23ee4b0215a48a93c8364e33a3f
Cooper Blake <the_analogkid@yahoo.com>
2004-12-13 04:53:32 +0000

    11/22/2004:  added class and web page to manage the login accounts in the accounting software.
    git-svn-id: http://localhost/svn/accounting/trunk@12 8c5786a8-5417-0410-96a1-a027eabcfbd0

---
+++ loginClass.php
@@ -0,0 +1,311 @@
+<?
+/*
+	Login class
+	Created 11/17/2004 by Cooper Blake
+
+	Purpose:
+		The management of login accounts
+
+	Notes:
+		As with other classes, the string member variables
+		must have quotation marks escaped.  The accessor methods
+		will strip these slashes.
+
+		The password variable will be blank when login is loaded from
+		database.  Only a non-blank password will be updated on save;
+		however, a non-blank password is required when the login_id is
+		-1 (for new login account).
+
+*/
+
+class Login
+{
+	private $m_login_id				= -1;
+	private $m_login_user			= '';
+	private $m_login_password		= '';
+	private $m_default_account_id	= -1;
+	private $m_default_summary1		= -1;
+	private $m_default_summary2		= -1;
+	private	$m_car_account_id		= -1;
+	private $m_login_admin			= 0;
+	private $m_display_name			= '';
+	private $m_active				= 1;
+
+	// ACCESSOR methods
+	public function get_login_id() {
+		return $this->m_login_id;
+	}
+	public function get_login_user() {
+		return stripslashes ($this->m_login_user);
+	}
+	public function get_login_password() {
+		return stripslashes ($this->m_login_password);
+	}
+	public function get_default_account_id ($bSql = false)
+	{
+		if ($bSql && $this->m_default_account_id == -1)
+			return 'NULL';
+
+		return $this->m_default_account_id;
+	}
+	public function get_default_summary1 ($bSql = false) 
+	{
+		if ($bSql && $this->m_default_summary1 == -1)
+			return 'NULL';
+
+		return $this->m_default_summary1;
+	}
+	public function get_default_summary2 ($bSql = false)
+	{
+		if ($bSql && $this->m_default_summary2 == -1)
+			return 'NULL';
+
+		return $this->m_default_summary2;
+	}
+	public function get_car_account_id($bSql = false)
+	{
+		if ($bSql && $this->m_car_account_id == -1)
+			return 'NULL';
+
+		return $this->m_car_account_id;
+	}
+	public function get_login_admin ($bText = false)
+	{
+		if ($bText) 
+		{
+			switch ($this->m_login_admin)
+			{
+				case 0:
+					return 'Normal user';
+				case 1:
+					return 'Account manager';
+				case 2:
+					return 'System manager';
+				default:
+					return 'Unknown user type';
+			}
+		}
+
+		return $this->m_login_admin;
+	}
+	public function get_display_name() {
+		return stripslashes ($this->m_display_name);
+	}
+	public function get_active ($bText = false) {
+		if ($bText)
+		{
+			if ($this->m_active == 1)
+				return 'Active';
+			else
+				return 'Inactive';
+		}
+		return $this->m_active;
+	}
+
+
+	/*
+		Purpose:
+			Initialize all required member variables; used to load
+			logins about to be saved or prepare update to existing login.
+		Returns:
+			an empty string on success, error message otherwise.
+		Preconditions:
+			1. All string fields must have quotation marks already escaped.
+		Post-conditions:
+			1. All member variables are bound to input values.
+	*/
+	public function Init_login (
+		$login_user,
+		$login_password,
+		$default_account_id,
+		$default_summary1,
+		$default_summary2,
+		$car_account_id,
+		$login_admin,
+		$display_name,
+		$login_id = -1,
+		$active = 1 )
+	{
+		$error = '';
+
+		// VALIDATE
+		if ($login_user == '')
+			$error = 'You must enter a username';
+		/*
+		elseif ($default_account_id < 0)
+			$error = 'Please select a default ledger account';
+		elseif ($default_summary1 < 0)
+			$error = 'Please select a first default summary account';
+		elseif ($default_summary2 < 0)
+			$error = 'Please select a second default summary account';
+		*/
+		elseif ($display_name == '')
+			$error = 'Please enter a display name';
+
+		$this->m_login_user			= $login_user;
+		$this->m_login_password		= $login_password;
+		$this->m_default_account_id	= $default_account_id;
+		$this->m_default_summary1	= $default_summary1;
+		$this->m_default_summary2	= $default_summary2;
+		$this->m_car_account_id		= $car_account_id;
+		$this->m_login_admin		= $login_admin;
+		$this->m_display_name		= $display_name;
+		$this->m_login_id			= $login_id;
+		$this->m_active				= $active;
+
+		return $error;
+	}
+
+
+	/*
+		Purpose:
+			Load data from database into this class.
+		Post:
+			Member vars are populated, with string quotations escaped.
+	*/
+	public function Load_login ($login_id)
+	{
+		$sql = "SELECT * FROM Logins ".
+			"WHERE login_id = $login_id";
+		db_connect();
+		$rs = mysql_query ($sql);
+		$error = db_error ($rs, $sql);
+		if ($error == '')
+		{
+			$row = mysql_fetch_array ($rs, MYSQL_ASSOC);
+			$this->m_login_id			= $row['login_id'];
+			$this->m_login_user			= addslashes ($row['login_user']);
+			$this->m_login_password		= '';
+			$this->m_default_account_id	= $row['default_account_id'];
+			$this->m_default_summary1	= $row['default_summary1'];
+			$this->m_default_summary2	= $row['default_summary2'];
+			$this->m_car_account_id		= $row['car_account_id'];
+			$this->m_login_admin		= $row['login_admin'];
+			$this->m_display_name		= addslashes ($row['display_name']);
+			$this->m_active				= $row['active'];
+		}
+		mysql_close();
+
+		return $error;
+	}
+
+
+	/*
+		Purpose:
+			Save current login to the database.  This can be an insert or update.
+	*/
+	public function Save_login ()
+	{	
+		if ($this->m_login_id > -1)
+		{
+			// UPDATE existing login
+			$sql = "UPDATE logins \n".
+				"SET login_user = '$this->m_login_user', ".
+				" default_account_id = {$this->get_default_account_id(true)}, ".
+				" default_summary1 = {$this->get_default_summary1(true)}, ".
+				" default_summary2 = {$this->get_default_summary2(true)}, ".
+				" car_account_id = {$this->get_car_account_id(true)}, ".
+				" login_admin = $this->m_login_admin, ".
+				" display_name = '$this->m_display_name', ".
+				" active = $this->m_active \n";
+			if ($this->m_login_password != '')
+			{
+				// user is updating the password
+				$sql.= ", login_password = MD5('$this->m_login_password')";
+			}
+			$sql.= "\n WHERE login_id = $this->m_login_id ";	
+		}
+		else
+		{
+			// INSERT new login
+			$sql = "INSERT INTO logins \n".
+				"(login_user, login_password, default_account_id, ".
+				" default_summary1, default_summary2, car_account_id, ".
+				" login_admin, display_name, active) \n".
+				"VALUES( '$this->m_login_user', MD5('$this->m_login_password'), ".
+				" {$this->get_default_account_id(true)}, ".
+				" {$this->get_default_summary1(true)}, ".
+				" {$this->get_default_summary2(true)}, ".
+				" {$this->get_car_account_id(true)}, ".
+				" $this->m_login_admin, '$this->m_display_name', ".
+				" $this->m_active )";
+		}
+	//return $sql;
+
+		db_connect();
+		$rs = mysql_query ($sql);
+		$error = db_error ($rs, $sql);
+		$affected = mysql_affected_rows();
+		mysql_close();
+
+		$msg = '';
+		if ($affected < 0)
+			$error = 'Unable to add or update login info.';
+		else
+		{
+			$msg = "Successfully updated $affected row";
+			if ($affected > 1)
+				$msg .= 's';
+		}
+		
+		return $error;
+	}
+
+
+
+	public function Delete_login ()
+	{
+		$error = '';
+		$sql = "DELETE FROM Logins \n".
+			"WHERE login_id = $this->m_login_id ";
+
+		db_connect();
+		$rs = mysql_query ($sql);
+		$error = db_error ($rs, $sql);
+		$affected = mysql_affected_rows();
+		mysql_close();
+
+		$msg = '';
+		if ($affected < 0)
+			$error = 'Could not find rows to delete.';
+		else
+		{
+			$msg = "Successfully deleted $affected row";
+			if ($affected > 1)
+				$msg .= 's';
+		}
+
+		return $error;
+	}
+
+
+	/*
+		Purpose:
+			Query database for list of all user logins.  The returned list
+			will be used to build a dropdown.
+	*/
+	public static function Get_login_list()
+	{
+		$sql = "SELECT login_id, login_user, display_name \n".
+			"FROM logins ";
+		$error = '';
+		$login_list = array();
+
+		db_connect();
+		$rs = mysql_query ($sql);
+		$error = db_error ($rs, $sql);
+		if ($error == '')
+		{
+			while ($row = mysql_fetch_array ($rs, MYSQL_ASSOC))
+			{
+				$login_list[$row['login_id']] = $row['login_user'].
+					" ($row[display_name])";
+			}
+		}
+		
+		mysql_close();
+		return $login_list;
+	}
+
+
+}	// END LOGIN CLASS
---
+++ logins.php
@@ -0,0 +1,251 @@
+<?
+/*
+	Login management page
+	Created 11/17/2004 by Cooper Blake
+	This allows admins to add, update & delete login accounts
+*/
+	$current_page = 'logins';
+	require('include.php');
+
+	$fixed_login_id = -1;
+	$login_id = -1;
+	if (!isset ($_SESSION['login_id']))
+	{
+		// redirect to login if they have not logged in
+		header ("Location: login.php");
+	}
+	elseif ($_SESSION['login_admin'] < 1)
+	{
+		// no admin permission; naughty!
+		header ("Location: login.php");
+	}
+	elseif ($_SESSION['login_admin'] == 1)
+	{
+		// limit user to only their own account
+		$fixed_login_id = $_SESSION['login_id'];
+		$login_id = $fixed_login_id;
+	}
+
+	$error = '';
+
+	// Default values
+	$login = new Login ();
+
+	$mode = '';
+	if (isset ($_POST['save']))
+		$mode = 'save';
+	elseif (isset ($_POST['delete']))
+		$mode = 'delete';
+	elseif (isset ($_POST['login_id']))
+		$mode = 'select';	//login account has been selected
+	// Form submit
+	if ($mode == 'save' || $mode == 'delete')
+	{
+		$active = 0;
+		if (isset ($_POST['active']))
+			$active = 1;
+
+		$error = $login->Init_login (
+			$_POST['login_user'],
+			$_POST['login_password'],
+			$_POST['default_account_id'],
+			$_POST['default_summary1'],
+			$_POST['default_summary2'],
+			$_POST['car_account_id'],
+			$_POST['login_admin'],
+			$_POST['display_name'],
+			$_POST['login_id'],
+			$active );
+
+		$login_id = $login->get_login_id();
+		if ($error == '')
+		{
+			if ($mode == 'save')
+			{
+				$error = $login->Save_login();
+			}
+			elseif ($mode == 'delete')
+				$error = $login->Delete_login();
+
+			if ($fixed_login_id == -1 && $error == '')
+			{
+				// reset display on success
+				$login = new Login ();
+				$login_id = -1;
+			}
+		}
+	}
+	elseif ($mode == 'select' && $fixed_login_id == -1)
+	{
+		$login_id = $_POST['login_id'];
+	}
+
+	if ($fixed_login_id > -1 || $mode == 'select' && $login_id > -1)
+	{
+		// fixed login OR new login selected
+		$login->Load_login ($login_id);
+	}
+
+	// Build dropdowns
+	$login_list = Login::Get_login_list();
+	$login_list = array('-1'=> '--Select login--') + $login_list;
+	$login_input = Build_dropdown ($login_list, 'login_id',
+		$login_id);
+	
+	$acc_list = Account::Get_account_list ($login_id);
+	$acc_list = array('-1'=> '--Select Account--') + $acc_list;
+	$defAcc_dropdown = Build_dropdown ($acc_list, 'default_account_id',
+		$login->get_default_account_id());
+	$summ1_dropdown = Build_dropdown ($acc_list, 'default_summary1',
+		$login->get_default_summary1());
+	$summ2_dropdown = Build_dropdown ($acc_list, 'default_summary2',
+		$login->get_default_summary2());
+	$car_dropdown = Build_dropdown ($acc_list, 'car_account_id',
+		$login->get_car_account_id());
+
+	$admin_list = array ('0'=>'Normal user',
+		'1'=>'Account manager',
+		'2'=>'System manager');
+	$admin_input = Build_dropdown ($admin_list, 'login_admin',
+		$login->get_login_admin());
+	$active_input = '<input type="checkbox" name="active" value="1"';
+	if ($login->get_active() == 1)
+		$active_input .= ' CHECKED';
+	$active_input .= '>';
+
+
+	if ($fixed_login_id > -1)
+	{
+		// replace dropdown with fixed text
+		$login_input = $login->get_login_user(). ' ('.
+			$login->get_display_name(). ') <input type="hidden" '.
+			'name="login_id" value="'. $fixed_login_id. '">';
+		$admin_input = $login->get_login_admin (true).
+			'<input type="hidden" name="login_admin" value="'.
+				$login->get_login_admin(). '">';
+		$active_input = $login->get_active(true).
+			' <input type="hidden" name="active" value="'.
+			$login->get_active(). '">';
+	}
+
+?>
+
+
+<html>
+<head>
+	<title>Login Management</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<script language="javascript" type="text/javascript">
+	
+		function select_account()
+		{
+			window.document.forms[0].submit();
+		}
+
+		function confirmDelete()
+		{
+			return confirm ('Are you sure you want to delete this login?');
+		}
+
+	</script>
+</head>
+<body>
+<?= $navbar ?>
+
+<form method="post" action="logins.php">
+<h3>Login Management</h3>
+<p class="error"><?= $error ?></p>
+<p>Please select your login</p>
+
+<table>
+	<tr>
+		<td>Login account:</td>
+		<td><?= $login_input ?></td>
+		<td><?
+			if ($fixed_login_id == -1)
+				echo '<input type="submit" name="select" value="Edit">';
+			?></td>
+	</tr>
+
+	<tr>
+		<td colspan="3"><hr></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>Username:</td>
+		<td><input type="text" name="login_user" size="25" maxlength="25"
+			value="<?= $login->get_login_user() ?>"></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>Display name:</td>
+		<td><input type="text" name="display_name" size="25" maxlength="50"
+			value="<?= $login->get_display_name() ?>"></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>Password:</td>
+		<td><input type="password" name="login_password" size="25" maxlength="25"
+			value="<?= $login->get_login_password() ?>"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Default ledger account:</td>
+		<td><?= $defAcc_dropdown ?></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>Default summary account 1:</td>
+		<td><?= $summ1_dropdown ?></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>Default summary account 2:</td>
+		<td><?= $summ2_dropdown ?> &nbsp;(used for Account Breakdown)</td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>Car expense account:</td>
+		<td><?= $car_dropdown ?> &nbsp;(fuel consumption statistics)</td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>User type:</td>
+		<td><?= $admin_input ?></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td>User status:</td>
+		<td><?= $active_input ?></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td colspan="2"><hr></td>
+	</tr>
+
+	<tr>
+		<td></td>
+		<td><input type="submit" name="save" value="Save"></td>
+		<td><?
+			if ($fixed_login_id == -1)
+			{
+				echo '<input type="submit" name="delete" onClick="return confirmDelete()" '.
+					'value="Delete">';
+			}
+			?></td>
+	</tr>
+
+
+</table>
+
+</body>
+</html>

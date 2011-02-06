1ff78dfb13c4397b96b69414cc709f9c856e8454
Cooper Blake <the_analogkid@yahoo.com>
2011-02-06 00:03:44 +0000

    Added database-backed account lockout support after too many bad logins.
    git-svn-id: http://localhost/svn/accounting/trunk@51 8c5786a8-5417-0410-96a1-a027eabcfbd0

--- login.php
+++ login.php
@@ -9,44 +9,18 @@
 	if (isset ($_POST['login_user']))
 	{
 		// attempted login
-		$login_user = $_POST['login_user'];
-		$login_password = $_POST['login_password'];
-		$sql = "SELECT login_id, default_account_id, login_admin, ".
-			"  display_name, default_summary1, default_summary2, ".
-			"  car_account_id \n".
-			"FROM Logins \n".
-			"WHERE login_user = '$login_user' ".
-			"  AND login_password = MD5('$login_password') ";
-		db_connect();
-		$rs = mysql_query ($sql);
-		if (!$rs)
+		$result = Login::Authenticate($_POST['login_user'], $_POST['login_password']);
+		if ($result === true)
 		{
-			$error = "Unable to query the database for login: ".
-				mysql_error();
+			// Redirect to main page
+			header ("Location: index.php");
 		}
 		else
 		{
-			if ($row = mysql_fetch_array ($rs))
-			{
-				// successful login
-				$_SESSION['login_id']			= $row['login_id'];
-				$_SESSION['login_user']			= $_POST['login_user'];
-				$_SESSION['default_account_id']	= $row['default_account_id'];
-				$_SESSION['default_summary1']	= $row['default_summary1'];
-				$_SESSION['default_summary2']	= $row['default_summary2'];
-				$_SESSION['car_account_id']		= $row['car_account_id'];
-				$_SESSION['login_admin']		= $row['login_admin'];
-				$_SESSION['display_name']		= $row['display_name'];
-
-				header ("Location: index.php");
-			}
-			else
-			{
-				// bad login
-				$error = "Incorrect username & password";
-			}
+			// bad login
+			$error = $result;
 		}
-		mysql_close();
+
 	}	// end login processing
 	elseif (isset ($_GET['logout']))
 	{
--- loginClass.php
+++ loginClass.php
@@ -29,7 +29,11 @@ class Login
 	private	$m_car_account_id		= -1;
 	private $m_login_admin			= 0;
 	private $m_display_name			= '';
+	private $m_bad_login_count		= 0;
+	private $m_locked				= 0;
 	private $m_active				= 1;
+
+	private static $MAX_AUTH_FAILURES = 5;
 
 	// ACCESSOR methods
 	public function get_login_id() {
@@ -91,6 +95,15 @@ class Login
 	public function get_display_name() {
 		return stripslashes ($this->m_display_name);
 	}
+
+	public function get_bad_login_count() {
+		return $this->m_bad_login_count;
+	}
+
+	public function get_locked() {
+		return $this->m_locked;
+	}
+
 	public function get_active ($bText = false) {
 		if ($bText)
 		{
@@ -182,6 +195,8 @@ class Login
 			$this->m_car_account_id		= $row['car_account_id'];
 			$this->m_login_admin		= $row['login_admin'];
 			$this->m_display_name		= addslashes ($row['display_name']);
+			$this->m_bad_login_count	= $row['bad_login_count'];
+			$this->m_locked				= $row['locked'];
 			$this->m_active				= $row['active'];
 		}
 		mysql_close();
@@ -207,6 +222,8 @@ class Login
 				" car_account_id = {$this->get_car_account_id(true)}, ".
 				" login_admin = $this->m_login_admin, ".
 				" display_name = '$this->m_display_name', ".
+				" bad_login_count = $this->m_bad_login_count, ".
+				" locked = $this->m_locked, ".
 				" active = $this->m_active \n";
 			if ($this->m_login_password != '')
 			{
@@ -305,6 +322,145 @@ class Login
 		
 		mysql_close();
 		return $login_list;
+	}
+
+
+	// Set the bad_login_count for the given user login_user.  Update
+	// the locked flag to 0 when $count is 0, or 1 when we exceed
+	// the bad login count.
+	public static function Set_bad_login_count($login_user, $count)
+	{
+		$locked = 0;
+		if ($count >= self::$MAX_AUTH_FAILURES)
+		{
+			$locked = 1;
+		}
+		$sql = "UPDATE Logins SET bad_login_count = $count, locked = $locked ".
+			"WHERE login_user = '$login_user'";
+		db_connect();
+		$result = mysql_query($sql);
+		$affected = mysql_affected_rows();
+		$error = '';
+		if ($result !== true)
+		{
+			$error = 'Error updating bad login count: ' . mysql_error();
+		}
+		else if ($affected != 1)
+		{
+			$error = "Got $affected affected rows instead of 1";
+		}
+
+		mysql_close();
+		return $error;
+	}
+
+
+	// Get the login id from the give login_user, or NULL
+	// if the user isn't found.
+	public static function Find_login_id($login_user)
+	{
+		$result = null;
+
+		$sql = "SELECT login_id FROM Logins WHERE login_user = '$login_user'";
+		db_connect();
+		$rs = mysql_query($sql);
+		if (!$rs)
+		{
+			$result = "Error querying login id: " . mysql_error();
+		}
+		else
+		{
+			if ($row = mysql_fetch_array($rs))
+			{
+				$result = $row['login_id'];
+			}
+		}
+
+		mysql_close();
+		return $result;
+	}
+
+
+	public static function Authenticate($user, $password)
+	{
+		$sql = "SELECT login_id, default_account_id, login_admin, ".
+			"  display_name, default_summary1, default_summary2, ".
+			"  car_account_id, bad_login_count, locked \n".
+			"FROM Logins \n".
+			"WHERE login_user = '$user' ".
+			"  AND login_password = MD5('$password') ";
+		$result = false;
+		$db = db_connect();
+		$rs = mysql_query ($sql);
+		if (!$rs)
+		{
+			$error = "Unable to query the database for login: ".
+				mysql_error();
+		}
+		else
+		{
+			if ($row = mysql_fetch_array ($rs))
+			{
+				// found login & correct password.  Check for lockout.
+				$locked = $row['locked'];
+				if ($locked > 0)
+				{
+					$result = "The account '$user' is locked!";
+				}
+				else
+				{
+					$_SESSION['login_id']			= $row['login_id'];
+					$_SESSION['login_user']			= $_POST['login_user'];
+					$_SESSION['default_account_id']	= $row['default_account_id'];
+					$_SESSION['default_summary1']	= $row['default_summary1'];
+					$_SESSION['default_summary2']	= $row['default_summary2'];
+					$_SESSION['car_account_id']		= $row['car_account_id'];
+					$_SESSION['login_admin']		= $row['login_admin'];
+					$_SESSION['display_name']		= $row['display_name'];
+
+					$result = true;
+
+					// on success, wipe the bad login count
+					self::Set_bad_login_count($user, 0);
+				}
+			}
+			else
+			{
+				// bad login
+				$result = "Incorrect username & password";
+
+				// find the relevant user login_id
+				$login_id = self::Find_login_id($user);
+
+				if (is_numeric($login_id) && $login_id > 0)
+				{
+					// Load user object, get bad count, then increment it.
+					$login = new Login();
+					$login->Load_login($login_id);
+					$bad_count = $login->get_bad_login_count();
+					$bad_count++;
+					$error = self::Set_bad_login_count($user, $bad_count);
+
+					if ($bad_count >= self::$MAX_AUTH_FAILURES)
+					{
+						$result = "The account '$user' has been locked!";
+					}
+
+					if ($error != '')
+					{
+						$result = $error;
+					}
+				}
+				else if (strlen($login_id) > 5)
+				{
+					$result = "Problem finding login_id: " . $login_id;
+				}
+			}  // End bad login
+		}	// End rs handling
+
+		mysql_close($db);
+
+		return $result;
 	}
 
 

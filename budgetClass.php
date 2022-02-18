142affab6d80adaeed30733cb07a5577b8da92a7
Cooper Blake <the_analogkid@yahoo.com>
2022-02-18 14:21:55 -0500

    Set PDO data type of Int

--- budgetClass.php
+++ budgetClass.php
@@ -88,7 +88,7 @@ class Budget {
 			foreach ($updateList as $batch) {
 				$ps->bindParam($i++, $batch->get_budget_amount());
 				$ps->bindParam($i++, $batch->get_budget_comment());
-				$ps->bindParam($i++, $batch->get_budget_id());
+				$ps->bindParam($i++, $batch->get_budget_id(), PDO::PARAM_INT);
 			}
 			$success = $ps->execute();
 			if (!$success) {
@@ -101,7 +101,7 @@ class Budget {
 			$ps = $pdo->prepare($insertSql);
 			$i = 1;
 			foreach ($insertList as $batch) {
-				$ps->bindParam($i++, $batch->get_account_id());
+				$ps->bindParam($i++, $batch->get_account_id(), PDO::PARAM_INT);
 				$ps->bindParam($i++, $batch->get_budget_month()->format('Y-m-d'));
 				$ps->bindParam($i++, $batch->get_budget_amount());
 				$ps->bindParam($i++, $batch->get_budget_comment());
